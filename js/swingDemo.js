bee717da72c6c2077f307348ccb349202a4e58e0
Chris Mondok <chris.mondok@gmail.com>
2015-04-18 15:16:30 -0400

    Don't use matter aliases

--- js/swingDemo.js
+++ js/swingDemo.js
@@ -7,15 +7,15 @@ require(['js/Level.js', 'js/Ball.js'], function(Level, Ball) {
 		SwingDemoLevel.prototype.init = function() {
 			base.init.apply(this, arguments);
 
-			var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-			var ceil = Bodies.rectangle(400, 0, 810, 60, { isStatic: true });
-			var left = Bodies.rectangle(0, 300, 60, 600, { isStatic: true });
-			var right = Bodies.rectangle(800, 300, 60, 600, { isStatic: true });
+			var ground = Matter.Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+			var ceil = Matter.Bodies.rectangle(400, 0, 810, 60, { isStatic: true });
+			var left = Matter.Bodies.rectangle(0, 300, 60, 600, { isStatic: true });
+			var right = Matter.Bodies.rectangle(800, 300, 60, 600, { isStatic: true });
 
-			var ballBody = Bodies.circle(400, 300, 5, {density:.005, restitution:0.5});
+			var ballBody = Matter.Bodies.circle(400, 300, 5, {density:.005, restitution:0.5});
 
 			var ball = new Ball(ballBody);
-			World.add(this.engine.world, [ground, ceil, left, right, ballBody]);
+			Matter.World.add(this.engine.world, [ground, ceil, left, right, ballBody]);
 			this.engine.fg = [];
 			this.engine.fg.push(ball);
 		}
