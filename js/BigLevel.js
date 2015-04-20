cac7d29f7cf1cb71dccb35e0af28b6b7079c4e0b
Chris Mondok <chris.mondok@gmail.com>
2015-04-20 01:15:12 -0400

    Give biglevel a floor

--- js/BigLevel.js
+++ js/BigLevel.js
@@ -1,11 +1,13 @@
-require(['js/Level.js', 'js/NavigationPoint.js', 'js/Player.js'], 
-function(Level, NavigationPoint, Player) {
+require(['js/Level.js', 'js/NavigationPoint.js', 'js/Player.js', 'js/Ball.js', 'js/Floor.js', 'js/Sand.js'], 
+function(Level, NavigationPoint, Player, Ball, Floor, Sand) {
 
 	function BigLevel() {
 		Level.apply(this, arguments);
 	}
 
 	BigLevel.inherits(Level, function(base) {
+
+		//BigLevel.prototype.bgm = "bgm4";
 
 		BigLevel.prototype.width = 3000;
 		BigLevel.prototype.height = 3000;
@@ -18,6 +20,20 @@ function(Level, NavigationPoint, Player) {
 			}
 
 			this.player = new Player(this, {x: 600, y: 400});
+
+			new Ball(this, {x: 650, y: 450});
+
+			for(var x = 0; x < this.width; x += this.width/10) {
+				for(var y = 0; y < this.height; y += this.height/10) {
+					var what = [Floor, Sand].randomize()[0];
+					new what(this, [
+						{x: x, y: y},
+						{x: x + this.width/10, y: y},
+						{x: x + this.width/10, y: y + this.height/10},
+						{x: x, y: y + this.height/10}
+					]);
+				}
+			}
 		};
 
 	});
